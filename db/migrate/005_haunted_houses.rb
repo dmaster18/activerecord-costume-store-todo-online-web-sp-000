5663a6a28ddf512c923751925a3873ce87329976
David Strauch <dmaster18@gmail.com>
2019-10-30 19:25:33 +0000

    Automatically backed up by Learn

--- db/migrate/005_haunted_houses.rb
+++ db/migrate/005_haunted_houses.rb
@@ -3,6 +3,6 @@
 class ModifyHauntedHouses < ActiveRecord::Migration[5.1]
   def change
     rename_column :haunted_houses, :family_friendly?, :family_friendly
-    rename_column :haunted_houses, :family_friendly?, :family_friendly
+    rename_column :haunted_houses, :long_description, :description
   end
 end
