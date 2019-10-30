a3e6d6b145119007962c62da53364b9f7e42fc13
David Strauch <dmaster18@gmail.com>
2019-10-30 19:25:22 +0000

    Automatically backed up by Learn

--- db/migrate/005_haunted_houses.rb
+++ db/migrate/005_haunted_houses.rb
@@ -3,6 +3,6 @@
 class ModifyHauntedHouses < ActiveRecord::Migration[5.1]
   def change
     rename_column :haunted_houses, :family_friendly?, :family_friendly
-    rename_column :costume_stores, :in_business?, :still_in_business
+    rename_column :haunted_houses, :family_friendly?, :family_friendly
   end
 end
