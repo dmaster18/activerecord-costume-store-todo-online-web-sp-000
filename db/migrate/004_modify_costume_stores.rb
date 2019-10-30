2efb0e6cce22f44e529058bcac92faa05bec90c6
David Strauch <dmaster18@gmail.com>
2019-10-30 19:20:06 +0000

    Automatically backed up by Learn

--- db/migrate/004_modify_costume_stores.rb
+++ db/migrate/004_modify_costume_stores.rb
@@ -4,12 +4,5 @@ class ModifyCostumeStores < ActiveRecord::Migration[5.1]
   def change
     rename_column :costume_stores, :number_of_employees, :num_of_employees
     rename_column :costume_stores, :in_business?, :still_in_business
-      t.string :name
-      t.string :location
-      t.integer :costume_inventory
-      t.integer :num_of_employees
-      t.boolean :still_in_business
-      t.datetime :opening_time
-      t.datetime :closing_time
   end
 end
