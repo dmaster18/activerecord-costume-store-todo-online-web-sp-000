9ac594633e08447f3acc3ea73e11a558d2c1281e
David Strauch <dmaster18@gmail.com>
2019-10-30 19:25:16 +0000

    Automatically backed up by Learn

--- db/migrate/005_haunted_houses.rb
+++ db/migrate/005_haunted_houses.rb
@@ -2,7 +2,7 @@
 
 class ModifyHauntedHouses < ActiveRecord::Migration[5.1]
   def change
-    rename_column :haunted_houses, :number_of_employees, :num_of_employees
+    rename_column :haunted_houses, :family_friendly?, :family_friendly
     rename_column :costume_stores, :in_business?, :still_in_business
   end
 end
